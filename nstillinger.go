408488b7e415713df5b6ab9a8d40a5a597f52fdc
tovare <mail@tovare.com>
2018-09-30 14:57:25 +0200

    Mock av tjeneste

---
+++ nstillinger.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"time"
+)
+
+func stillinger(w http.ResponseWriter, r *http.Request) {
+	// TODO: Unmarshall
+	mock := `{"stillinger":"000","annonser":"000","nye":"0"}`
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Write([]byte(mock))
+}
+
+func main() {
+	var (
+		portnummer string
+		prefix     string
+	)
+	flag.StringVar(&portnummer, "p", ":8085", "Hvilket portnummer/adresse")
+	flag.StringVar(&prefix, "prefix", "/api", "Hvilket adresse ligger løsningen på")
+	flag.Parse()
+
+	{
+		ticker := time.NewTicker(5000 * time.Millisecond)
+		go func() {
+			for t := range ticker.C {
+				log.Println("Oppdaterte data ", t)
+			}
+		}()
+	}
+
+	checkErr := func(err error) {
+		if err != nil {
+			log.Fatal("ERROR:", err)
+		}
+	}
+	http.HandleFunc(prefix+"/stillinger", stillinger) // set router
+	err := http.ListenAndServe(portnummer, nil)       // set listen port
+	checkErr(err)
+	log.Println("API stillinger, lytter til adresse  ", portnummer)
+}
